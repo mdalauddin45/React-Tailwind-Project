558bf4984cc9b6dd3a05492fb1a43a2663640bac
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-05 19:17:09 +0600

    nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -18,9 +18,8 @@ const NavBar = () => {
       <div onClick={() => setOpen(!open)} className="h-6 w-6 md:hidden">
         {open ? <XMarkIcon /> : <Bars3Icon />}
       </div>
-
       <ul
-        className={`bg-purple-200 w-full md:flex justify-center absolute duration-500 ease-in  ${
+        className={`bg-purple-200 md:flex w-full justify-center absolute md:static duration-500 ease-in ${
           open ? "top-6" : "top-[-120px]"
         }`}
       >
